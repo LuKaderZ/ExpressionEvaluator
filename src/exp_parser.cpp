14970bb0908947c9410a4b6632f73d56dd60f097
xuweilun <xuweilun571_02@outlook.com>
2021-08-28 19:44:32 +0800

    feat:表达式内运算符检查

--- src/exp_parser.cpp
+++ src/exp_parser.cpp
@@ -1,7 +1,49 @@
 #include "expression.h"
+#include <map>
 
 using namespace LL;
 using std::string;
+using std::map;
+
+struct Operator_Property
+{
+	int priority;
+	int operator_position;
+	int parameter_count;
+};
+map<string, Operator_Property> operators
+{
+	{"ROOT",{-1,0,1}},
+	{"+",{0,2,2}},
+	{"++",{5,1,1}},
+	{"-",{0,2,2}},
+	{"--",{5,1,1}},
+	{"*",{1,2,2}},
+	{"/",{1,2,2}},
+	{"%",{1,2,2}},
+	{"%%",{5,3,1}},
+	{"^",{2,2,2}},
+	{"!",{4,3,1}},
+	{"sqrt",{5,1,1}},
+	{"abs",{5,1,1}},
+	{"lg",{5,1,1}},
+	{"ln",{5,1,1}},
+	{"sin",{5,1,1}},
+	{"cos",{5,1,1}},
+	{"tan",{5,1,1}},
+	{"arcsin",{5,1,1}},
+	{"arccos",{5,1,1}},
+	{"arctan",{5,1,1}},
+	{"sinh",{5,1,1}},
+	{"cosh",{5,1,1}},
+	{"tanh",{5,1,1}},
+	{"arsinh",{5,1,1}},
+	{"arcosh",{5,1,1}},
+	{"artanh",{5,1,1}},
+	{"min",{3,1,2}},
+	{"max",{3,1,2}},
+	{"log",{3,1,2}}
+};
 
 void Expression::parser(const string& expression)
 {
@@ -36,4 +78,23 @@ void Expression::parser(const string& expression)
 		}
 		return cells_temp;
 	}();
+
+	try
+	{
+		for (auto i : *cells)
+		{
+			if (('A' <= (*i)[0] && (*i)[0] <= 'Z') || ('a' <= (*i)[0] && (*i)[0] <= 'z'))
+			{
+				if (operators.find(*i) == operators.end())
+				{
+					string error = "unknown operator:" + *i;
+					throw error.c_str();
+				}
+			}
+		}
+	}
+	catch (const char* ERROR_INFORMATION)
+	{
+		throw ERROR_INFORMATION;
+	}
 }
