89e4e7ee39343d2397288cd8e1c47a3ab7b4356b
xuweilun <xuweilun571_02@outlook.com>
2021-08-28 20:08:00 +0800

    feat:解析表达式树

--- src/exp_parser.cpp
+++ src/exp_parser.cpp
@@ -45,6 +45,120 @@ map<string, Operator_Property> operators
 	{"log",{3,1,2}}
 };
 
+Expression::node* Expression::to_tree(Exp_Cells* cells)
+{
+	if (cells->empty())
+		return nullptr;
+
+	node* ROOT = new node;
+	ROOT->data = "ROOT";
+	ROOT->father = ROOT;
+	ROOT->parameter_count = 1;
+
+	auto get_priority = [](const string& str) {return operators.find(str)->second.priority; };
+	auto get_operator_position = [](const string& str) {return operators.find(str)->second.operator_position; };
+	auto get_parameter_count = [](const string& str) {return operators.find(str)->second.parameter_count; };
+
+	node* current_father = ROOT;
+	for (Exp_Cells::const_iterator iter = cells->begin(); iter != cells->end(); iter++)
+	{
+		node* temp = new node;
+		if (**iter == "SKIP")
+			continue;
+		if ('0' <= (**iter)[0] && (**iter)[0] <= '9')
+		{
+			temp->is_operator = false;
+			temp->data = **iter;
+
+			temp->father = current_father;
+			current_father->children.push_back(temp);
+		}
+		else if (**iter == "(")
+		{
+			delete temp;
+			iter++;
+			Exp_Cells* new_tokens = new Exp_Cells;
+			int Left_bracket = 0;
+			while (true)
+			{
+				if (**iter == "(")
+					Left_bracket++;
+				else if (**iter == ")")
+					Left_bracket--;
+				if (Left_bracket == -1)
+				{
+					temp = to_tree(new_tokens);
+					delete new_tokens;
+					for (auto i : temp->children)
+					{
+						current_father->children.push_back(i);
+						i->father = current_father;
+					}
+					delete temp;
+					break;
+				}
+				else
+					new_tokens->push_back(*iter);
+				iter++;
+			}
+			continue;
+		}
+		else if (get_priority(**iter) == 3 || get_priority(**iter) == 5)
+		{
+			temp->father = current_father;
+			current_father->children.push_back(temp);
+			temp->data = **iter;
+			temp->parameter_count = get_parameter_count(**iter);
+			current_father = temp;
+		}
+		else
+		{
+			if ((current_father->data == "ROOT") && current_father->children.size() == 0)
+			{
+				temp->father = current_father;
+				temp->data = **iter;
+				temp->parameter_count = get_parameter_count(**iter);
+				current_father->children.push_back(temp);
+			}
+			else
+			{
+				if (get_priority(**iter) > get_priority(current_father->data))
+				{
+					temp->father = current_father;
+					temp->data = **iter;
+					temp->parameter_count = get_parameter_count(**iter);
+					switch (get_operator_position(**iter))
+					{
+					case 2:
+					case 3:
+						temp->children.push_back(current_father->children.back());
+						current_father->children.back()->father = temp;
+						current_father->children.pop_back();
+						current_father->children.push_back(temp);
+						break;
+					}
+				}
+				else
+				{
+					temp->father = current_father->father;
+					temp->data = **iter;
+					temp->parameter_count = get_parameter_count(**iter);
+					temp->children.push_back(current_father);
+					current_father->father->children.pop_back();
+					current_father->father->children.push_back(temp);
+					current_father->father = temp;
+				}
+			}
+			current_father = temp;
+		}
+
+		if (current_father->children.size() == current_father->parameter_count)
+			current_father = current_father->father;
+	}
+
+	return ROOT;
+}
+
 void Expression::parser(const string& expression)
 {
 	Exp_Cells* cells = [&expression]() {
@@ -97,4 +211,6 @@ void Expression::parser(const string& expression)
 	{
 		throw ERROR_INFORMATION;
 	}
+
+	ROOT = to_tree(cells);
 }
--- src/expression.h
+++ src/expression.h
@@ -11,6 +11,18 @@ namespace LL
 	public:
 		void parser(const std::string& expression);
 	private:
+		struct node
+		{
+			node* father = nullptr;
 
+			bool is_operator = true;
+			std::string data = "";
+			int parameter_count = 0;
+
+			std::vector<node*> children;
+		};
+		node* ROOT;
+
+		node* to_tree(Exp_Cells* cells);
 	};
 }
